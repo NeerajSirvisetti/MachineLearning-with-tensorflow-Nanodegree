28175fdc4ab4092fb7fdfc09a4fe8365841bda57
NeerajSirvisetti <32873782+NeerajSirvisetti@users.noreply.github.com>
2020-04-14 19:43:31 +0530

    Add files via upload

---
+++ Deep Learning/project/workspace/predict (1).py
@@ -0,0 +1,48 @@
+import argparse
+import tensorflow as tf
+import tensorflow_hub as hub
+import json
+from PIL import Image
+from process_image import process_image
+import numpy as np
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("x")
+parser.add_argument("y")
+parser.add_argument("--top_k",  type=int, required=False,default=1)
+parser.add_argument("--category_names",  required=False,default = 'label_map.json')
+args = vars(parser.parse_args())
+im = Image.open(args['x'])
+test_image = np.asarray(im)
+
+processed_test_image = process_image(test_image)
+processed_test_image=np.expand_dims(processed_test_image, axis=0)
+
+topk=args['top_k']
+model = tf.keras.models.load_model(args['y'],custom_objects={'KerasLayer':hub.KerasLayer})
+result=model.predict(processed_test_image)
+cls=tf.math.top_k(result,topk)[1].numpy()[0]
+prob=tf.math.top_k(result,topk)[0].numpy()[0]
+
+if(args['category_names']):
+    with open(args['category_names'], 'r') as f:
+        class_names = json.load(f)
+    
+
+lists = [] 
+for key in class_names.keys():
+    lists.append(int(key))
+lists.sort()
+
+classes=[]
+for i in cls:
+    classes.append(class_names[str(lists[i])])
+probs=[]
+
+for i in prob:
+    probs.append(i)
+
+for i in range(len(probs)):
+    print(classes[i],":",probs[i])
+
